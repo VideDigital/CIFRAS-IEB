3447b14e0f971d13c4d93c67450728bc9ba31a50
SilviaCostaCookier <danielmarcelino549@gmail.com>
2026-07-26 08:30:12 -0300

    Criar firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBqisXinkDuP7DU8JP0D2avWJnDMdoVr3o",
+  authDomain: "cifras-ieb.firebaseapp.com",
+  projectId: "cifras-ieb",
+  storageBucket: "cifras-ieb.firebasestorage.app",
+  messagingSenderId: "687479137832",
+  appId: "1:687479137832:web:cd4e5c87b6b79e6e0c771d",
+  measurementId: "G-RM9N6KFNKM"
+};
